74760d89fc3ee01225213ecebeb3d8a452be66fd
taeyeonyoo <taeyeonyoo16@gmail.com>
2025-02-16 00:44:59 +0900

    feat: 푸터 퍼블리싱(#5)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div className='link-wrapper'>
+                <div>About Soongsil Times</div>
+                <div>Contact Us</div>
+                <div>Terms of Use</div>
+                <div>Privacy Policy</div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
