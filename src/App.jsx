e3e638c987419cfbd44400217fa3a4648ad265f1
taeyeonyoo <taeyeonyoo16@gmail.com>
2025-02-16 01:23:54 +0900

    feat: 페이지 레이아웃 설정(#5)

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,25 @@ import Article from './pages/Article';
 import Edit from './pages/Edit';
 import Search from './pages/Search';
 import Notfound from './pages/Notfound';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path="/category/:name" element={<Category />} />
-      <Route path='/article/:id' element={<Article/>} />
-      <Route path='/edit/:id' element={<Edit/>} />
-      <Route path='/search' element={<Search/>} />
-      <Route path='*' element={<Notfound/>} />
-    </Routes>
-    </>
+    <div className='App'>
+      <Header/>
+      <div className='content'>
+        <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path="/category/:name" element={<Category />} />
+        <Route path='/article/:id' element={<Article/>} />
+        <Route path='/edit/:id' element={<Edit/>} />
+        <Route path='/search' element={<Search/>} />
+        <Route path='*' element={<Notfound/>} />
+      </Routes>
+      </div>
+      <Footer/>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,7 @@
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-
 const Home = () => {
     return (
         <div>
-            <Header/>
-            <Footer/>
+
         </div>
     );
 };
