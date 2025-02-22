9e7a342b45f4de6181b8aa35993b1ba72a9f6994
taeyeonyoo <taeyeonyoo16@gmail.com>
2025-02-23 02:58:36 +0900

    feat: 네비바 동작 구현(#5)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
 import './Header.css';
 import searchIcon from '../assets/search.svg';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const Header = () => {
+    const navigate = useNavigate();
+    const [navState, setNavState] = useState("HOME");
+
+    const handleNavClick = (category) => {
+    setNavState(category);
+    if (category === "HOME") {
+        navigate("/")
+    } else {
+        navigate(`/category/${category.toLowerCase()}`);
+    }};
+
+    const categories = ["HOME", "CAMPUS", "SOCIETY", "BUSINESS", "SCIENCE", "CULTURE", "ARCHIVE"];
+
     return (
         <div>
             <div className='header'>
@@ -11,13 +26,14 @@ const Header = () => {
             <div className='nav-wrapper'>
                 <div className='nav'>
                     <div className='nav-link'>
-                        <div>HOME</div>
-                        <div>CAMPUS</div>
-                        <div>SOCIETY</div>
-                        <div>BUSINESS</div>
-                        <div>SCIENCE</div>
-                        <div>CULTURE</div>
-                        <div>ARCHIEVE</div>
+                        {categories.map((category) => (
+                            <div
+                                key={category}
+                                onClick={() => handleNavClick(category)}>
+                                {category}
+                                {navState == category && <div className='selected '/>}
+                            </div>
+                        ))}
                     </div>
 
                     <div className='search-container'>
