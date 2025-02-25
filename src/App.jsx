a3f0f14f9f010f6f214a0fcb0bd2bd15b83e3a8b
taeyeonyoo <taeyeonyoo16@gmail.com>
2025-02-26 03:21:19 +0900

    feat: 기사 관리 기능 구현(#6)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { useReducer, useRef } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Category from './pages/Category';
@@ -9,7 +10,74 @@ import Notfound from './pages/Notfound';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    category: "CAMPUS",
+    content: "1번 기사 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    category: "SOCIETY",
+    content: "2번 기사 내용",
+  },
+]
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'CREATE':
+      return [action.data,...state]
+    case 'UPDATE':
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item);
+    case 'DELETE':
+      return state.filter((item) =>
+        String(item.id) !== String(action.id));
+      default:
+        return state;
+  }
+}
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  // 새로운 기사 추가
+  const onCreate = (createdDate, category, content) => {
+    dispatch ({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        category,
+        content,
+      }
+    });
+  }
+
+  // 기존 기사 수정
+  const onUpdate = (id, createdDate, category, content) => {
+    dispatch ({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        category,
+        content,
+      }
+    });
+  }
+
+  // 기존 기사 삭제
+  const onDelete = (id) => {
+    dispatch ({
+      type: "DELETE",
+      id,
+    });
+  }
+
   return (
     <div className='App'>
       <Header/>
