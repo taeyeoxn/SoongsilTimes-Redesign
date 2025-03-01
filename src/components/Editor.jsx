4f807a32e94ec9247237f939e9a60bb579f91995
taeyeonyoo <taeyeonyoo16@gmail.com>
2025-03-02 04:49:34 +0900

    feat: 날짜 저장 기능 구현(#9)

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,7 +1,45 @@
 import './Editor.css';
 import attachIcon from '../assets/attach.svg'
+import { useState } from 'react';
+
+// createdDate 프로퍼티에 저장된 Date 객체를 문자열로 변환
+const getStringedDate = (targetDate) => {
+    let year = targetDate.getFullYear();
+    let month = targetDate.getMonth() + 1;
+    let date = targetDate.getDate();
+
+    if (month < 10) {
+        month = `0${month}`;
+    }
+    if (date < 10) {
+        date = `0${date}`;
+    }
+
+    return `${year}-${month}-${date}`;
+};
 
 const Editor = () => {
+    const [input, setInput] = useState({
+        createdDate: new Date(),
+        title: "",
+        reporter: "",
+        content: "",
+    });
+
+    const onChangeInput = (e) => {
+        let name = e.target.name;
+        let value = e.target.value;
+
+        if (name === 'createdDate') {
+            value = new Date(value); 
+        }
+    
+        setInput({
+            ...input,
+            [name]: value,
+        });
+    };
+    
     return (
         <div>
             <div className='title-input'>
@@ -10,7 +48,11 @@ const Editor = () => {
             <div className='sub-input'>
                 <input placeholder='Enter the Author...'/>
                 <div className='sub-input-bar'></div>
-                <div className='article-date'>2025.03.01.</div>
+                <input 
+                name='createdDate'
+                onChange={onChangeInput}
+                value={getStringedDate(input.createdDate)} 
+                type='date'/>
             </div>
             <div className='content-input'>
                 <div className='article-image'>
